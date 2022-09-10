57df3d87f96f4235e2ad398d520b109644ab28a4
Matthias Leutenegger <mleutenegger@users.noreply.github.com>
2022-09-10 16:50:54 +0200

    add: correctly transfer a graphql static dir (#36)
    * add: correctly transfer a graphql static dir
    
    * update molecule tests
    
    * check webroot
    
    * fix path
    
    * gdmt
    
    * build graphql
    
    * update php
    
    * pfreqs
    
    * php
    
    * php
    
    * deps
    
    * update
    
    * ...
    
    * no webserver
    
    * no webserver

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,3 +13,18 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+    f = host.file('/var/www/html/index.php')
+    assert f.exists
+
+def test_webroot(host):
+    f = host.file('/var/www/html/')
+    assert f.exists
+    f = host.file('/var/www/html/index.php')
+    assert f.exists
+    f = host.file('/var/www/html/_graphql')
+    assert f.exists
+    f = host.file('/var/www/html/assets')
+    assert f.exists
+    f = host.file('/var/www/html/.htaccess')
+    assert f.exists
